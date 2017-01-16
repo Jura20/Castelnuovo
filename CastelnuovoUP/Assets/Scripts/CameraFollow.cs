35f215d454ad66045c4ad9529192de2bfc24b379
miquelgalianallorca <miquelgalianallorca@gmail.com>
2017-01-16 16:02:29 +0100

    Player control
    Player movement and collision, Camera movement.

---
+++ CastelnuovoUP/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform followedObject;
+    Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    private float cameraDelay = .3f;
+    [SerializeField]
+    private float cameraHeight = 8f;
+    [SerializeField]
+    private float cameraDepth = 3f;
+
+    void Start()
+    {
+        if (followedObject == null) Debug.LogError("Camera doesn't have a target.");
+    }
+
+	void LateUpdate () {
+        Vector3 targetPosition = followedObject.position;
+        targetPosition.y += cameraHeight;
+        targetPosition.z -= cameraDepth;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, cameraDelay);
+    }
+}
---
+++ CastelnuovoUP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform graphics;
+
+    [SerializeField]
+    private float moveSpeed = 3f;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField]
+    private float rotationSpeed = 5f;
+    [SerializeField]
+    private string collidablesLayerName = "Collidable";
+
+    //Components
+    private Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        if (graphics == null) Debug.LogError("Player graphics not set up.");
+    }
+	
+	void Update () {
+        Move();        
+    }
+
+    private void Move()
+    {
+        //Input
+        float xMov = Input.GetAxis("Horizontal");
+        float zMov = Input.GetAxis("Vertical");
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
+
+        //Animation movement
+        //...
+        
+        //Movement
+        velocity = (movHorizontal + movVertical) * moveSpeed;
+        rb.MovePosition(rb.position + velocity * Time.deltaTime);
+
+        //Rotation (graphics only)
+        if (velocity != Vector3.zero)
+        {
+            graphics.transform.rotation = Quaternion.Slerp(
+                graphics.transform.rotation,
+                Quaternion.LookRotation(velocity),
+                Time.deltaTime * rotationSpeed
+            );
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer(collidablesLayerName)) velocity = Vector3.zero;
+    }
+}
